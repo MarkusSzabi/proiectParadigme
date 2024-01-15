7abae63906e86f1832b030692d93f908ee28fa42
Markus L. Szabolcs <cti22b115@cst.uoradea.local>
2024-01-15 19:42:02 +0200

    adaugare laboratoare

---
+++ Laborator4/src/main/java/Problema4/Problema4.java
@@ -0,0 +1,8 @@
+package Problema4;
+
+public class Problema4 {
+    public static void main(String[] args) {
+        Rectangle dreptughi = new Rectangle(3 ,  2);
+        System.out.println("Dreptunghi => Arie: " + dreptughi.arie() + " Perimetru: " + dreptughi.perimetru());
+    }
+}
